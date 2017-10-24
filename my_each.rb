01cd5cb95184d24b82f6da77e12dc30ab7a5938f
Chris Roberts <chrisrobertspdx@gmail.com>
2017-10-24 17:38:07 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,15 @@
-def my_each # put argument(s) here
+def my_each(arr) # put argument(s) here
   # code here
+  if block_given?
+    i=0
+    while i < arr.size
+      yield i
+      i =+ 1
+    end
+    arr
+  else
+    puts "No block was given."
+  end
 end
+
+my_each([0,1,2,3]) {|i| puts i}
